126013c45fa4863a7959f02591c268521ec8f6ac
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-10 00:30:02 +0200

    Time: 70ms (83.90%), Space: 44.8Mb (32.21%)

--- 74. Search a 2D Matrix/SearchMatrix.ts
+++ 74. Search a 2D Matrix/SearchMatrix.ts
@@ -1,5 +1,35 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
-    return false;
+    let row = searchRows(0, matrix.length - 1, target, matrix);
+    return BS(0, matrix[row].length - 1, target, matrix[row]);
 };
+
+const searchRows = (start: number, end: number, target: number, matrix: number[][]): number => {
+    if (start > end) {
+        return end < 0 ? start : end;
+    }
+    const mid = Math.floor((start + end) / 2);
+    if (matrix[mid][0] === target) {
+        return mid;
+    }
+    if (matrix[mid][0] > target) {
+        return searchRows(start, mid - 1, target, matrix);
+    }
+    return searchRows(mid + 1, end, target, matrix);
+}
+
+
+const BS = (start: number, end: number, target: number, matrix: number[]): boolean => {
+    if (start > end) {
+        return false;
+    }
+    const mid = Math.floor((start + end) / 2);
+    if (matrix[mid] === target) {
+        return true;
+    }
+    if (matrix[mid] > target) {
+        return BS(start, mid - 1, target, matrix);
+    }
+    return BS(mid + 1, end, target, matrix);
+}
 
 export default searchMatrix;
