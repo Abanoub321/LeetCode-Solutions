44be013e21353f089c6fe620c87f9fc6e49c1141
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-09 23:44:53 +0200

    Time: 85ms (66.62%), Space: 44.9MB (41.44%)

--- 19. Remove Nth Node From End of List/RemoveNthFromEnd.ts
+++ 19. Remove Nth Node From End of List/RemoveNthFromEnd.ts
@@ -13,7 +13,19 @@
 import ListNode from "../Interfaces/ListNode";
 
  function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-    return head;
+    let dummy = new ListNode(0);
+    let slow = dummy, fast = dummy;
+    dummy.next = head;
+    while (n--) {
+        fast = fast.next!;
+    }
+    while (fast.next) {
+        slow = slow.next!;
+        fast = fast.next!;
+    }
+    slow.next = slow.next!.next;
+    return dummy.next;
+
 };
 
 export default removeNthFromEnd;
