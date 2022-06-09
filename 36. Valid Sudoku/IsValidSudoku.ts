ce69034d0da8de56c51c63730bce06b30ba62327
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-09 11:04:02 +0200

    Time: 95ms (77.86%),Space: 48.3MB (24.71%)

--- 36. Valid Sudoku/IsValidSudoku.ts
+++ 36. Valid Sudoku/IsValidSudoku.ts
@@ -1,33 +1,14 @@
 function isValidSudoku(board: string[][]): boolean {
-    let map: { [key: string]: true } = {};
+    let map: { [key: string]: boolean } = {};
+
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            const c = board[i][j];
-            if (c === '.') continue;
-            if (map[c]) return false;
-            map[c] = true;
-        }
-        map = {};    
-    }
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            const c = board[j][i];
-            if (c === '.') continue;
-            if (map[c]) return false;
-            map[c] = true;
-        }
-        map = {};
-    }
-    
-    for (let i = 0; i < 9; i += 3) {
-        for (let j = 0; j < 9; j += 3) {
-            for (let k = 0; k < 9; k++) {
-                const c = board[i + Math.floor(k / 3)][j + k % 3];
-                if (c === '.') continue;
-                if (map[c]) return false;
-                map[c] = true;
-            }
-            map = {};
+            const val = board[i][j];
+            if (val === '.') continue;
+            if (map[`${val}r${i}`] || map[`${val}c${j}`] || map[`${val}b${Math.floor(i / 3)}-${Math.floor(j / 3)}`]) return false;
+            map[`${val}r${i}`] = true;
+            map[`${val}c${j}`] = true;
+            map[`${val}b${Math.floor(i / 3)}-${Math.floor(j / 3)}`] = true;
         }
     }
 
