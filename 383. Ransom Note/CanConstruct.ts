00819ab5332b3f760da5c6ba0e13a06e9faa3bc8
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-10 14:49:25 +0200

    Time: 119ms (57.38%), Space: 46.1MB (59.88%)

--- 383. Ransom Note/CanConstruct.ts
+++ 383. Ransom Note/CanConstruct.ts
@@ -1,4 +1,11 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
+    const map: { [key: string]: number } = {};
+    for (let i = 0; i < magazine.length; i++)
+        map[magazine[i]] = (map[magazine[i]] || 0) + 1;
+    for (let i = 0; i < ransomNote.length; i++) {
+        if (!map[ransomNote[i]]) return false;
+        map[ransomNote[i]]--;
+    }
     return true;
 };
 
