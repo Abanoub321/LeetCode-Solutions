9fb355b366baf861295211dd6d9835f482d64533
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-12 11:58:31 +0200

    More Test for clarity

--- 1658. Minimum Operations to Reduce X to Zero/MinOperations.test.ts
+++ 1658. Minimum Operations to Reduce X to Zero/MinOperations.test.ts
@@ -12,4 +12,7 @@ describe('Minimum operations to reduce X to Zero', () => {
     it('pass test 3', () => {
         expect(minOperations([3, 2, 20, 1, 1, 3], 10)).toBe(5);
     })
+    it('pass test 4', () => {
+        expect(minOperations([8828, 9581, 49, 9818, 9974, 9869, 9991, 10000, 10000, 10000, 9999, 9993, 9904, 8819, 1231, 6309], 134365)).toBe(16);
+    })
 })
