4f2feb3e1c9375fd3e848bed7b027cdb9e103b4b
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-12 14:30:06 +0200

    Time: 292ms (58.33%),Space: 69.6MB (41.67%)

--- 1695. Maximum Erasure Value/MaximumUniqueSubarray.ts
+++ 1695. Maximum Erasure Value/MaximumUniqueSubarray.ts
@@ -1,5 +1,22 @@
 function maximumUniqueSubarray(nums: number[]): number {
-    return 0;
+    let max = 0;
+    let sum = 0;
+    let i = 0;
+    let j = 0;
+    let set: Set<number> = new Set();
+    while (i < nums.length && j < nums.length) {
+        if (set.has(nums[j])) {
+            sum -= nums[i];
+            set.delete(nums[i]);
+            i++;
+        } else {
+            set.add(nums[j]);
+            sum += nums[j];
+            max = Math.max(max, sum);
+            j++;
+        }
+    }
+    return max;
 };
 
 export default maximumUniqueSubarray;
