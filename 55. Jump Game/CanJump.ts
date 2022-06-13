7d1f7bda07f0200358dca81ed15825015324c71f
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-13 15:46:35 +0200

    Time: 89ms (85.92%),Space: 50.7MB (20.98%)

--- 55. Jump Game/CanJump.ts
+++ 55. Jump Game/CanJump.ts
@@ -1,5 +1,15 @@
 function canJump(nums: number[]): boolean {
-    return true;
+    if (nums.length === 1) return true;
+    if (nums[0] === 0) return false;
+    let dp: number[] = [];
+    dp[0] = nums[0];
+    for (let i = 1; i < nums.length - 1; i++) {
+        if (dp[i - 1] < i)
+            return false;
+        dp[i] = Math.max(dp[i - 1], nums[i] + i);
+    }
+    dp[nums.length - 1] = dp[nums.length - 2];
+    return dp[nums.length - 1] >= nums.length - 1;
 };
 
 export default canJump;
