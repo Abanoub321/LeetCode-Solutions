988c1ae4c6de924999b1c65fc4c9700f3c2d9f6f
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-14 15:35:14 +0200

    Time: 92ms (69.57%), Space: 48.7Mb (78.26%)

--- 918. Maximum Sum Circular Subarray/MaxSubarraySumCircular.ts
+++ 918. Maximum Sum Circular Subarray/MaxSubarraySumCircular.ts
@@ -1,5 +1,14 @@
 function maxSubarraySumCircular(nums: number[]): number {
-    return nums.length;
+    let currMax ,currMin,maxSum,minSum,sum;
+    currMax = currMin = maxSum = minSum = sum = nums[0];
+    for(let i = 1; i < nums.length; i++){
+        sum += nums[i];
+        currMax = Math.max(currMax + nums[i], nums[i]);
+        currMin = Math.min(currMin + nums[i], nums[i]);
+        maxSum = Math.max(maxSum, currMax);
+        minSum = Math.min(minSum, currMin);
+    }
+    return minSum === sum ? maxSum : Math.max(maxSum, sum - minSum);
 };
 
 export default maxSubarraySumCircular;
