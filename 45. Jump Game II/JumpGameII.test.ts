2f138748985a673fd239a71bb02b1e51656814eb
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-14 10:00:25 +0200

    Adding more test cases for clarity

--- 45. Jump Game II/JumpGameII.test.ts
+++ 45. Jump Game II/JumpGameII.test.ts
@@ -7,4 +7,7 @@ describe('Jump Game II', () => {
     it('pass test case 2', () => {
         expect(jump([2, 3, 0, 1, 4])).toBe(2);
     })
+    it('pass test case 3', () => {
+        expect(jump([3, 2, 1])).toBe(1);
+    })
 })
