50135ccf76ce65e3c082d4209abc2d0aa2577465
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-14 17:35:12 +0200

    Time: 143ms (13.03%),Space: 45Mb (57.98%)

--- 152. Maximum Product Subarray/MaxProduct.ts
+++ 152. Maximum Product Subarray/MaxProduct.ts
@@ -1,5 +1,15 @@
 function maxProduct(nums: number[]): number {
-    return nums.length;
+    let currMax = nums[0];
+    let currMin = nums[0];
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        let temp = Math.max(nums[i], nums[i] * currMax, nums[i] * currMin);
+        currMin = Math.min(nums[i], nums[i] * currMax, nums[i] * currMin)
+        currMax = temp;
+        max = Math.max(currMax, max);
+    }
+    return max;
 };
 
 export default maxProduct;
