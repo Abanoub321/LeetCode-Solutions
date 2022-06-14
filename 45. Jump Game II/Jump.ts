1bb0b899f106b4b7852b841813ef516d589a8c64
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-14 10:01:01 +0200

    Time: 67ms (98.98%), Space: 44.9MB (81.67%)

--- 45. Jump Game II/Jump.ts
+++ 45. Jump Game II/Jump.ts
@@ -1,5 +1,16 @@
 function jump(nums: number[]): number {
-    return nums.length;
+    let maxReach = 0;
+    let steps = 0;
+    let curEnd = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        maxReach = Math.max(maxReach, i + nums[i]);
+        if (i == curEnd) {
+            steps++;
+            curEnd = maxReach;
+        }
+    }
+
+    return steps;
 };
 
 export default jump;
