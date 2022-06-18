cf42b1f132e6af1d815923f12b90e909a4ffb7e2
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-18 23:03:27 +0200

    Time: 125ms (35.39%),Space: 44.6MB (88.34%)

--- 7. Reverse Integer/Reverse.ts
+++ 7. Reverse Integer/Reverse.ts
@@ -1,5 +1,16 @@
 function reverse(x: number): number {
-    return x;
+    let strNum = x.toString();
+    let result = 0; 
+    if (strNum[0] === '-') {
+        strNum = strNum.slice(1);
+        result = -1 * parseInt(strNum.split('').reverse().join(''));
+    } else {
+        result = parseInt(strNum.split('').reverse().join(''));
+    }
+    if (result < -2147483648 || result > 2147483647) {
+        return 0;
+    }
+    return result;
 };
 
 export default reverse;
