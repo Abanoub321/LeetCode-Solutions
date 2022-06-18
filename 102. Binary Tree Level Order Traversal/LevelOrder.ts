6135bb274d8871267415c966e54e92c7eaa29ece
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-18 15:34:39 +0200

    Time: 71ms (94.04%),Space: 45.4MB (25.51%)

--- 102. Binary Tree Level Order Traversal/LevelOrder.ts
+++ 102. Binary Tree Level Order Traversal/LevelOrder.ts
@@ -1,7 +1,18 @@
 import TreeNode from "../Interfaces/TreeNode";
 
 function levelOrder(root: TreeNode | null): number[][] {
-    return [[]]
+    let arr: number[][] = [];
+    if (root === null) return arr;
+    traverse(root, 0, arr);
+    return arr;
 };
+
+const traverse = (root: TreeNode | null, level: number, arr: number[][]): void => {
+    if (root === null) return;
+    if (arr[level] === undefined) arr[level] = [];
+    arr[level].push(root.val);
+    traverse(root.left, level + 1, arr);
+    traverse(root.right, level + 1, arr);
+}
 
 export default levelOrder;
