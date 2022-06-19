432ed1288456ba0cb71450fa11fec3bddd46e6b4
Abanoub321 <abanoubmilad12@gmail.com>
2022-06-19 23:50:51 +0200

    Time: 104ms (68.61%), Space: 44.3MB (98.54%)

--- 8. String to Integer (atoi)/myAtoi.ts
+++ 8. String to Integer (atoi)/myAtoi.ts
@@ -1,5 +1,12 @@
 function myAtoi(s: string): number {
-    return s.length;
+    let result = parseInt(s, 10);
+    if(isNaN(result)) 
+        return 0;
+    if(result > Math.pow(2, 31) - 1)
+        return Math.pow(2, 31) - 1;
+    if(result < -Math.pow(2, 31))
+        return -Math.pow(2, 31);
+    return result;
 };
 
 export default myAtoi;
