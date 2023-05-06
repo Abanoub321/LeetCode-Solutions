f37040db2a745e832210fcac48ada6906fb284da
abanoub321 <abanoubmilad12@gmail.com>
2023-05-06 22:40:37 +0300

    add new case with timelimit

--- 473. Matchsticks to Square/makesquare.test.ts
+++ 473. Matchsticks to Square/makesquare.test.ts
@@ -8,4 +8,14 @@ describe('473. Matchsticks to Square', () => {
     it('should pass test case 2', () => {
         expect(makesquare([3, 3, 3, 3, 4])).toBe(false);
     });
+
+    it('should pass test case 3 without timelimit', () => {
+        let timelimit = 2000;
+        let start = Date.now();
+        expect(makesquare([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 102]
+        )).toBe(false);
+
+        let end = Date.now();
+        expect(end - start).toBeLessThan(timelimit);
+    });
 });
