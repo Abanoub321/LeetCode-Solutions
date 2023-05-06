92a0f3dde851e4a3f2925f3cc2029fa15949bd2a
abanoub321 <abanoubmilad12@gmail.com>
2023-05-06 21:51:37 +0300

    more test cases

--- 1498. Number of Subsequences That Satisfy the Given Sum Condition/numSubseq.ts
+++ 1498. Number of Subsequences That Satisfy the Given Sum Condition/numSubseq.ts
@@ -1,34 +1,24 @@
-
-/**
-  nums = [3,5,6,7], target = 9
-  [3]
-  [3, 5]
-    [3, 5, 6]
-    [3,  6]
-
-    result will be 4 as there are 4 subsequence that satisfy the condition
-    get the number of subsequence that satisfy the condition
- */
 function numSubseq(nums: number[], target: number): number {
     let result = 0;
     const mod = 10 ** 9 + 7;
-    
+
     const sortedNums = nums.sort((a, b) => a - b);
 
-    for(let i = 0; i < sortedNums.length; i++) {
-        let j = 0;
-
-        while(j < sortedNums.length && sortedNums[i] + sortedNums[j] <= target) {
-            j++;
+    let j = sortedNums.length - 1;
+    for (let i = 0; i < sortedNums.length; i++) {
+        while (i <= j && sortedNums[i] + sortedNums[j] > target) {
+            j--;
         }
-
-        if(j > i) {
-            result += (2 ** (j - i - 1)) % mod;
-            result %= mod;
+        if (j >= i) {
+            result = (result + power(2, j - i, mod)) % mod;
         }
     }
 
     return result;
 };
+
+const power = (base: number, exponent: number, mod: number): number => {
+    return exponent === 0 ? 1 : (base * power(base, exponent - 1, mod)) % mod;
+}
 
 export default numSubseq;
