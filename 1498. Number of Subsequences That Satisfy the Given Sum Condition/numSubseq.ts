00723454120b426c060ce583323f33998c6d9910
abanoub321 <abanoubmilad12@gmail.com>
2023-05-06 21:56:24 +0300

    Time: 215ms (33.33%),Space: 55.1Mb (100%)

--- 1498. Number of Subsequences That Satisfy the Given Sum Condition/numSubseq.ts
+++ 1498. Number of Subsequences That Satisfy the Given Sum Condition/numSubseq.ts
@@ -3,6 +3,11 @@ function numSubseq(nums: number[], target: number): number {
     const mod = 10 ** 9 + 7;
 
     const sortedNums = nums.sort((a, b) => a - b);
+    const power = new Array(sortedNums.length);
+
+    for (let i = 0; i < sortedNums.length; i++) {
+        power[i] = i === 0 ? 1 : (power[i - 1] * 2) % mod;
+    }
 
     let j = sortedNums.length - 1;
     for (let i = 0; i < sortedNums.length; i++) {
@@ -10,15 +15,11 @@ function numSubseq(nums: number[], target: number): number {
             j--;
         }
         if (j >= i) {
-            result = (result + power(2, j - i, mod)) % mod;
+            result = (result + power[j - i]) % mod;
         }
     }
 
     return result;
 };
-
-const power = (base: number, exponent: number, mod: number): number => {
-    return exponent === 0 ? 1 : (base * power(base, exponent - 1, mod)) % mod;
-}
 
 export default numSubseq;
