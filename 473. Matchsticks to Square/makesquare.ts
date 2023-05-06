985d22beb0cec1cc45333d5d6905d38f9c4900bf
abanoub321 <abanoubmilad12@gmail.com>
2023-05-06 22:24:14 +0300

    Passing default test cases

--- 473. Matchsticks to Square/makesquare.ts
+++ 473. Matchsticks to Square/makesquare.ts
@@ -1,5 +1,24 @@
 function makesquare(matchsticks: number[]): boolean {
-    return false;
+    const sum = matchsticks.reduce((a, b) => a + b, 0);
+    if (sum % 4 !== 0) return false;
+    const side = sum / 4;
+    const sides = [0, 0, 0, 0];
+    return dfs(matchsticks, sides, side, 0);
 };
+
+function dfs(matchsticks: number[], sides: number[], side: number, index: number): boolean {
+    if (index === matchsticks.length) {
+        return sides.every(side => side === side);
+    }
+
+    for (let i = 0; i < 4; i++) {
+        if (sides[i] + matchsticks[index] > side) continue;
+        sides[i] += matchsticks[index];
+        if (dfs(matchsticks, sides, side, index + 1)) return true;
+        sides[i] -= matchsticks[index];
+    }
+
+    return false;
+}
 
 export default makesquare;
