0df943c98c8ac03b50947922620b00d3cdc0b82b
Abanoub321 <48459005+Abanoub321@users.noreply.github.com>
2024-01-19 14:11:32 +0200

    Time: 77ms (37.14%), Space: 53.46 Mb (5.71%)

--- 931. Minimum Falling Path Sum/minFallingPathSum.ts
+++ 931. Minimum Falling Path Sum/minFallingPathSum.ts
@@ -1,5 +1,34 @@
 function minFallingPathSum(matrix: number[][]): number {
-  return 1;
+  let dp: number[][] = [];
+  let n: number = matrix.length;
+  let m: number = matrix[0].length;
+  let min: number = Infinity;
+
+  for (let i = 0; i < n; i++) {
+    dp[i] = [];
+  }
+
+  for (let i = 0; i < m; i++) {
+    dp[0][i] = matrix[0][i];
+  }
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      dp[i][j] =
+        matrix[i][j] +
+        Math.min(
+          dp[i - 1][j],
+          dp[i - 1][j - 1] ?? Infinity,
+          dp[i - 1][j + 1] ?? Infinity
+        );
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    min = Math.min(min, dp[n - 1][i]);
+  }
+  
+  return min;
 }
 
 export default minFallingPathSum;
